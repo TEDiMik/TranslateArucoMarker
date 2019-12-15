6f2427a679c242ce53a7bd477056819fa772d23e
Сергей <121krolik11@gmail.com>
2019-12-15 12:45:34 +0500

    Understand OpenCv on Python

--- TranslateMarker.py
+++ TranslateMarker.py
@@ -6,7 +6,7 @@ import httplib2
 #import matplotlib.pyplot as plt
 #import matplotlib as mpl
 
-import numpy as np
+import numpy
 import cv2, PIL
 from cv2 import aruco
 import matplotlib.pyplot as plt
@@ -15,9 +15,7 @@ import pandas as pd
 
 import numpy as np
 import cv2
-import cv2.aruco as aruco
 import glob
-
 
 '''
 h = httplib2.Http('.cache')
@@ -27,21 +25,53 @@ out.write(content)
 out.close()
 '''
 
-frame = cv2.imread("index.png")
+frame = cv2.imread("img.jpg")
+
+#----------------------------------------------------------------------
+'''
+distCoeffs=[0.0189223469433419, -0.0206788674793396,0.003225513523750, 0.001510000668961]
+cameraMatrix=[[600.8293, 0, 330.2756],[0, 601.3519, 225.9791],[0, 0, 1.0000]]
+
+
+img = frame
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+aruco_dict = cv2.aruco_Dictionary.get(aruco.DICT_4X4_1000)
+parameters = cv2.aruco_DetectorParameters.create()
+corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+cv2.aruco.drawDetectedMarkers(img,corners,ids,)
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 0.045, cameraMatrix, distCoeffs)
+cv2.aruco.drawAxis(img, cameraMatrix, distCoeffs, rvecs, tvecs, 0.1)
+'''
+#----------------------------------------------------------------------
+
+#frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+
+
+
+aruco_dict = aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+
 parameters = aruco.DetectorParameters_create()
 parameters.adaptiveThreshConstant = 10
+
+
 corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
+
+
+
 if np.all(ids != None):
 
-    frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+    
 
+    frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 
     plt.figure()
     plt.imshow(frame_markers)
@@ -57,7 +87,6 @@ else:
     cv2.putText(frame, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
     plt.imshow(frame)
     plt.show()
-
 
 
 
@@ -79,6 +108,20 @@ plt.imshow(frame_markers)
 plt.show()
 '''
 
+'''
+def MarkerDetection4x4(path):
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    aruco_dict = cv2.aruco_Dictionary.get(aruco.DICT_4X4_1000)
+    parameters = cv2.aruco_DetectorParameters.create()
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    cv2.aruco.drawDetectedMarkers(img,corners,ids,)
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 0.045, cameraMatrix, distCoeffs)
+    cv2.aruco.drawAxis(img, cameraMatrix, distCoeff, rvecs, tvecs, 0.1)
+'''
 
 
 print('Все')
