ee7d3bf02ce6a9e82eecaafe0be604c98eb1fc5f
Сергей <121krolik11@gmail.com>
2019-12-22 12:07:00 +0500

    Add Comments and clean up code

--- TranslateMarker.py
+++ TranslateMarker.py
@@ -17,6 +17,7 @@ import numpy as np
 import cv2
 import glob
 
+#Loading image by link
 '''
 h = httplib2.Http('.cache')
 response, content = h.request('https://stepik.org/media/attachments/lesson/284187/example_2.jpg')
@@ -24,55 +25,35 @@ out = open('img.jpg', 'wb')
 out.write(content)
 out.close()
 '''
-def calculateDistance(x1,y1,x2,y2):  
-     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
-     return dist  
+
+
+
 
 
 
 frame = cv2.imread("Sample/img2.jpg")
 
-hsv_min = np.array((-10, 171, 133))
-hsv_max = np.array((10, 191, 213))
+
+hsv_min = np.array((-7, 123, 116))
+hsv_max = np.array((13, 198, 196))
 color_yellow = (0,255,255)
-#----------------------------------------------------------------------
-'''
-distCoeffs=[0.0189223469433419, -0.0206788674793396,0.003225513523750, 0.001510000668961]
-cameraMatrix=[[600.8293, 0, 330.2756],[0, 601.3519, 225.9791],[0, 0, 1.0000]]
 
 
 
-
-
-
-img = frame
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-aruco_dict = cv2.aruco_Dictionary.get(aruco.DICT_4X4_1000)
-parameters = cv2.aruco_DetectorParameters.create()
-corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-cv2.aruco.drawDetectedMarkers(img,corners,ids,)
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 0.045, cameraMatrix, distCoeffs)
-cv2.aruco.drawAxis(img, cameraMatrix, distCoeffs, rvecs, tvecs, 0.1)
-'''
-#----------------------------------------------------------------------
-
-#frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
-
+#Convert 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+#Find red point 
 thresh = cv2.inRange(hsv, hsv_min, hsv_max)
 
+#Find Aruco marcer, White Square 4X4
 aruco_dict = aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
 
+#In documentation write what need
 parameters = aruco.DetectorParameters_create()
-#parameters.adaptiveThreshConstant = 7
 
-
+#angle markers/find markers
 corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -87,24 +68,20 @@ sum_y = moments['m01']
 sum_x = moments['m10']
 sum_pixel = moments['m00']
 
-if sum_pixel > 50:
+if sum_pixel > 5:
+    #Coordinate red point object
     x = int(sum_x / sum_pixel)
     y = int(sum_y / sum_pixel)
 
-
     frameCOlor1 = cv2.line(frame,(x,y),(0,0),(255,0,0),5)
-
-    
     plt.imshow(frameCOlor1)
-
-
-    #Рисуем круг в центр светового питна.
+    #Paint circle in coordinate red point
     cv2.circle(frame, (x, y), 10, (0, 0, 255), -1)
-    # Пишем координаты
+    # write coordinate
     cv2.putText(frame, "%d-%d" % (x, y), (x + 50, y - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color_yellow, 2)
 
 
-
+# if markers found 
 if np.all(ids != None):
 
     
@@ -127,8 +104,8 @@ if np.all(ids != None):
     x1 = int (corners[2][0][0][0])
     y1 = int (corners[2][0][0][1])
 
-    dist = calculateDistance(x,y,x1,y1)
-    print('DISTANCE: ', dist)
+    #dist = calculateDistance(x,y,x1,y1) #In old code
+    #print('DISTANCE: ', dist)
 
     frameCOlor = cv2.line(frame_markers,(x,y),(0,0),(255,0,0),5)
     frameCOlor = cv2.line(frame_markers,(x1,y1),(0,0),(255,0,0),5)
@@ -143,13 +120,44 @@ if np.all(ids != None):
 
 
 else:
-    # code to show 'No Ids' when no markers are found
+    # else write No markers
     cv2.putText(frame, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
     plt.imshow(frame)
     plt.show()
 
 
 
+
+#old code
+#
+'''
+def calculateDistance(x1,y1,x2,y2):  
+     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
+     return dist  
+'''
+# #----------------------------------------------------------------------
+'''
+distCoeffs=[0.0189223469433419, -0.0206788674793396,0.003225513523750, 0.001510000668961]
+cameraMatrix=[[600.8293, 0, 330.2756],[0, 601.3519, 225.9791],[0, 0, 1.0000]]
+
+
+
+
+
+
+img = frame
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+aruco_dict = cv2.aruco_Dictionary.get(aruco.DICT_4X4_1000)
+parameters = cv2.aruco_DetectorParameters.create()
+corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+cv2.aruco.drawDetectedMarkers(img,corners,ids,)
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 0.045, cameraMatrix, distCoeffs)
+cv2.aruco.drawAxis(img, cameraMatrix, distCoeffs, rvecs, tvecs, 0.1)
+'''
+#----------------------------------------------------------------------
 '''
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
@@ -166,7 +174,7 @@ plt.imshow(frame_markers)
 
 
 plt.show()
-'''
+''' 
 
 '''
 def MarkerDetection4x4(path):
@@ -184,5 +192,15 @@ def MarkerDetection4x4(path):
 '''
 
 
+
+
+
+
+
+
+
+
+#End programm
+#For my self
 print('Все')
 
